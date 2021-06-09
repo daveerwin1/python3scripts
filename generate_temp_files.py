a263d68b405aab3e37e02ff3e268115359ec0a18
Dave Erwin <derwin@concentricsky.com>
2021-06-09 13:51:33 -0700

    Added: changes to migration scripts

--- generate_temp_files.py
+++ generate_temp_files.py
@@ -1,8 +1,10 @@
 import os
 
+# Use `rm **/temp.txt` to delete all the temp.txt files after running the script to generate html files.
+
 for subdir, dirs, files in os.walk(os.getcwd()):
     for file in files:
-        if 'config.js' in file:
+        if '.scss' in file:
             open(subdir + '/temp.txt', 'w').close()
 
 
--- modaux_generate_html.py
+++ modaux_generate_html.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import re
 from bs4 import BeautifulSoup
 from bs4 import Comment
+from pprint import pprint
 
 # Usage: make files named temp.html with the output from the html pane in fractal
 # temp.html should be located in same dir as hbs files
@@ -28,39 +30,48 @@ for subdir, dirs, files in os.walk(os.getcwd()):
     for file in files:
         if file == 'temp.txt':
             full_filepath = os.path.join(subdir, file)
-            path = subdir
+            if os.stat(full_filepath).st_size > 0:
+                path = subdir
 
-            current_dir = subdir.split(os.path.sep)[-1]
-            with open(full_filepath, 'r') as f:
-                contents = f.read()
-                soup = BeautifulSoup(contents, 'lxml')
-                body = soup.find("body")
-                children = body.findChildren(recursive=False)
-                comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-                for num, name in enumerate(children):
-                    # Use the comment to name the html file.
-                    filename = path + '/' + str(comments[num].lower().strip().replace(" ", "-")) + '.html'
-                    file_contents = '---' + '\n'
-                    file_contents += 'title: ' + comments[num].strip() + '\n'
-                    file_contents += 'weight: ' + str(num + 1) + '\n'
-                    file_contents += '---' + '\n'
-                    file_contents += str(children[num].prettify().strip())
+                current_dir = subdir.split(os.path.sep)[-1]
+                with open(full_filepath, 'r') as f:
+                    contents = f.read()
+                    soup = BeautifulSoup(contents, 'html.parser')
+                    #body = soup.find("body")
+                    #children = soup.findChildren(recursive=False)
+                    soup1 = str(soup).strip('').replace('\n', '')
+                    children = re.split('<!--.*?-->', soup1)
+                    # first item in array is empty string, remove it
+                    children.pop(0)
+                    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+                    if not comments:
+                        comments = ["default"]
+                        body = soup.find("body")
+                        children = [str(soup)]
+                    for num, name in enumerate(children):
+                        # Use the comment to name the html file.
+                        filename = path + '/' + str(comments[num].lower().strip().replace(" ", "-").replace(":", "").replace("+", "")) + '.html'
+                        file_contents = '---' + '\n'
+                        file_contents += 'title: ' + comments[num].strip().replace(":", "") + '\n'
+                        file_contents += 'weight: ' + str(num + 1) + '\n'
+                        file_contents += '---' + '\n'
+                        file_contents += BeautifulSoup(children[num].strip(), 'html.parser').prettify()
 
-                    html_file = open(filename, 'w')
-                    html_file.write(file_contents)
-                    html_file.close()
+                        html_file = open(filename, 'w')
+                        html_file.write(file_contents)
+                        html_file.close()
 
-                    index_file_contents = '---' + '\n'
-                    index_file_contents += 'title: ' + current_dir.capitalize() + '\n'
-                    index_file_contents += 'modaux:' + '\n'
-                    index_file_contents += '  merge: true' + '\n'
-                    index_file_contents += '---'
+                        index_file_contents = '---' + '\n'
+                        index_file_contents += 'title: ' + current_dir.capitalize() + '\n'
+                        index_file_contents += 'modaux:' + '\n'
+                        index_file_contents += '  merge: true' + '\n'
+                        index_file_contents += '---'
 
 
-                    index_filename = path + '/' + '_index.md'
-                    index_file = open(index_filename, 'w')
-                    index_file.write(index_file_contents)
-                    index_file.close()
+                        index_filename = path + '/' + '_index.md'
+                        index_file = open(index_filename, 'w')
+                        index_file.write(index_file_contents)
+                        index_file.close()
 
 
 
